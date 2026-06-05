dd44376120c7b91711497534722f3a1f58937680
Essayas Abraham Hailu <80784659+tedy-abr@users.noreply.github.com>
2026-06-05 13:08:14 +0200

    Installed vite, react and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-amber-400">
+        Hello World
+      </h1>
+    </>
+  );
+}
+
+export default App;
